0bd301affdd8919b229e7389a9d1ee00420138a5
luka2220 <42144047+luka2220@users.noreply.github.com>
2022-02-12 11:05:36 -0500

    Assignment 2 added

--- Assignments/Assignment 2/Code/Assignment 2/Assignment 2/Program.cs
+++ Assignments/Assignment 2/Code/Assignment 2/Assignment 2/Program.cs
@@ -1,4 +1,37 @@
-﻿using System;
+﻿/*
+ 
+Program Documentation
+
+Name: Luka Piplica
+Student Number: 0748533
+
+Program Description
+This program was written for the purpose of computing the
+averages of the Biology, Computer Science, and Physics
+departments at Trent University. The program prompts the
+user to enter their deparment code, 'B' or 'b' for Biology, 
+'C' or 'c' for Computer Science, 'P' or 'p' for Physics, and
+'Q' or 'q' to quit the program and view the averages. The user 
+is then prompted to enter the mark from 0 - 100, which will be 
+used to compute the averages for each mark in the given department.
+Once the user quits the program they will be able to view the
+average marks for each department they entered.
+
+Data Dictionary
+deptCode: A variable of type char to store the deparment code the user enters, and will assign the store the grade based on the input
+mark: A variable of type double to store the mark the user inputs, and will be added to the according department mark total
+biologyMarkAverage: A variable of type double to store the avreage of biology department marks, its value is biologyMarkTotal / biologyCounter
+biologyMarkTotal: A variable of type double which is used to calculate avg. for the biology department
+biologyCounter: A variable of type int which is incremented by 1 every time the biology department code is entered, and is used to calculate avg. for the biology department
+computerScienceMarkAverage: A variable of type double to store the average of computer science marks, its value is computerScienceMarkTotal / computerScienceCounter
+computerScienceMarkTotal: A variable of type double which is for the calculation of the computer science deparment average
+computerScienceCounter: A variable of type int which is the number of marks inputted by the user for the cs dept., and is used to calculate the average for the cs. department
+physicsMarkAverage: A variable of type double which store the average grades for the physics department, its value is calculated by physicsMarkTotal / physicsCounter
+physicsMarkTotal: A variable of type double which is used for the calculation of the physics deparment grade average
+physicsCounter: A variable of type int which is the amount of grades inputted for the physics dept., and used to calculate the average grade for the physics department
+*/
+
+using System;
 public static class Assignment2
 {
     public static void Main()
@@ -26,15 +59,15 @@ public static class Assignment2
         // Counter variable for amount of marks added to physicsMarkTotal
         int physicsCounter = 0;
 
-        // Prompting user input of department code
-        Console.Write("Enter a department code: ‘C’ or ‘c’ for Computer Science,‘B’ or ‘b’ for History, " +
+        // Prompting user to input department code
+        Console.Write("Enter a department code: ‘C’ or ‘c’ for Computer Science,‘B’ or ‘b’ for Biology, " +
             "‘P’ or ‘p’ for Physics, or enter ‘Q’ or ‘q’ to quit: ");
         deptCode = Convert.ToChar(Console.ReadLine());
 
 
-        //sentinal loop to prompt user for department code and mark
+        // Sentinal loop to prompt user for department code and mark
         while (char.ToUpper(deptCode) != 'Q')
-        {
+        {            
             // Loop for validating grade input
             do
             {
@@ -42,7 +75,7 @@ public static class Assignment2
                 Console.Write("Enter the mark (between 0 - 100): ");
                 mark = Convert.ToDouble(Console.ReadLine());
             } while (mark < 0 || mark > 100);
-           
+
 
             // Add mark to department total based on department code
             if (char.ToUpper(deptCode) == 'B')
@@ -66,9 +99,14 @@ public static class Assignment2
                 // Increasing physics counter by 1
                 physicsCounter += 1;
             }
+            else 
+            {
+                // Prompting error for user to input correct department code
+                Console.Write("Error, Enter a correct department code\n");
+            }
 
             // Prompting user input of department code
-            Console.Write("Enter a department code: ‘C’ or ‘c’ for Computer Science,‘B’ or ‘b’ for History, " +
+            Console.Write("Enter a department code: ‘C’ or ‘c’ for Computer Science,‘B’ or ‘b’ for Biology, " +
                 "‘P’ or ‘p’ for Physics, or enter ‘Q’ or ‘q’ to quit: ");
             deptCode = Convert.ToChar(Console.ReadLine());
         }
